68ff49db8ddc3eb79c37c8dca40754ecbb6dcdec
Anmol <109303519+Anmol-002@users.noreply.github.com>
2024-02-09 22:09:37 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -96,6 +96,7 @@ const logout = catchAsyncError(async (req, res, next) => {
     expires: new Date(Date.now()),
     httpOnly: true,
   });
+  localStorage.removeItem('token');
 
   res.status(200).json({
     success: true,
